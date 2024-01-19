076a0f7433288810f92682eee8c759ad2704103d
ReedOtto0 <153955823+ReedOtto0@users.noreply.github.com>
2024-01-18 17:16:09 -0700

    Cleaned up slider gradients

--- app/components/ColorPicker/Slider.js
+++ app/components/ColorPicker/Slider.js
@@ -10,48 +10,33 @@ export default function Slider({ type }) {
   const color = useContext(ColorContext);
   const sliderClasses = `w-full py-1 ${styles.slider}`;
 
-  const currentColorHSL =
-    color.type === "HSL"
-      ? [color.data.h, color.data.s, color.data.l]
-      : color.type === "RGB"
-      ? RGBtoHSL(color.data.r, color.data.g, color.data.b)
-      : color.type === "HEX"
-      ? HEXtoHSL(color.data.r, color.data.g, color.data.b)
-      : [0, 0, 0];
+  const colorCSS = getCSS(color);
+  const solid = `radial-gradient(closest-side, ${colorCSS}, ${colorCSS})`;
+  const thumbRing = `radial-gradient(closest-side, transparent 0%, transparent 65%, black 65%, black 100%)`;
+  const thumbBG = `${thumbRing}, ${solid}`;
 
-  const currentColorRGB =
-    color.type === "HSL"
-      ? HSLtoRGB(color.data.h, color.data.s, color.data.l)
-      : color.type === "RGB"
-      ? [color.data.r, color.data.g, color.data.b]
-      : color.type === "HEX"
-      ? HEXtoRGB(color.data.r, color.data.g, color.data.b)
-      : [0, 0, 0];
+  const step = 60;
+  const hueStops = Array.from({ length: 360 / step + 1 }, (v, i) => i * step)
+    .map((hue) => ` hsl(${hue}, ${color.s}%, ${color.l}%)`)
+    .toString();
+  const hue = `linear-gradient(to right,${hueStops})`;
 
-  const currentColorCSS = getCSS(color);
-  const currentColorSolidBG = `radial-gradient(closest-side, ${currentColorCSS}, ${currentColorCSS})`;
-  const thumbRing = `radial-gradient(closest-side, transparent 0%, transparent 65%, black 66%, black 100%)`;
-  const thumbBG = `${thumbRing}, ${currentColorSolidBG}`;
+  const saturation = `linear-gradient(to right, hsl(${color.h}, 0%, ${color.l}%), hsl(${color.h}, 100%, ${color.l}%))`;
 
-  const linearGradientHue = `linear-gradient(to right, hsla(0, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(60, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(120, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(180, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(240, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(300, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1), hsla(360, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1))`;
-  const linearGradientSaturation = `linear-gradient(to right, hsl(${currentColorHSL[0]}, 0%, ${currentColorHSL[2]}%), hsl(${currentColorHSL[0]}, 100%, ${currentColorHSL[2]}%))`;
-  const linearGradientlightness = `linear-gradient(to right, hsl(${currentColorHSL[0]}, ${currentColorHSL[1]}%, 0%), hsl(${currentColorHSL[0]}, ${currentColorHSL[1]}%, 50%), hsl(${currentColorHSL[0]}, ${currentColorHSL[1]}%, 100%))`;
-  const linearGradientAlpha = `linear-gradient(to right, hsla(${currentColorHSL[0]}, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 0), hsla(${currentColorHSL[0]}, ${currentColorHSL[1]}%, ${currentColorHSL[2]}%, 1))`;
+  const lightness = `linear-gradient(to right, hsl(${color.h}, ${color.s}%, 0%), hsl(${color.h}, ${color.s}%, 50%), hsl(${color.h}, ${color.s}%, 100%))`;
+
+  const linearGradientAlpha = `linear-gradient(to right, hsla(${color.h}, ${color.s}%, ${color.l}%, 0), hsla(${color.h}, ${color.s}%, ${color.l}%, 1))`;
   const checkerboardGradient =
-    "repeating-conic-gradient(#808080 0% 25%, white 0% 50%) 50% / 16px 16px";
+    "repeating-conic-gradient(#808080 0% 25%, white 0% 50%) 50% / 12px 12px";
 
-  const linearGradientR = `linear-gradient(to right, rgba(0, ${currentColorRGB[1]}, ${currentColorRGB[2]}, 1), rgba(255, ${currentColorRGB[1]}, ${currentColorRGB[2]}, 1))`;
-  const linearGradientG = `linear-gradient(to right, rgba(${currentColorRGB[0]}, 0, ${currentColorRGB[2]}, 1), rgba(${currentColorRGB[0]}, 255, ${currentColorRGB[2]}, 1))`;
-  const linearGradientB = `linear-gradient(to right, rgba(${currentColorRGB[0]}, ${currentColorRGB[1]}, 0, 1), rgba(${currentColorRGB[0]}, ${currentColorRGB[1]}, 255, 1))`;
+  const linearGradientR = `linear-gradient(to right, rgba(0, ${color.g}, ${color.b}, 1), rgba(255, ${color.g}, ${color.b}, 1))`;
+  const linearGradientG = `linear-gradient(to right, rgba(${color.r}, 0, ${color.b}, 1), rgba(${color.r}, 255, ${color.b}, 1))`;
+  const linearGradientB = `linear-gradient(to right, rgba(${color.r}, ${color.g}, 0, 1), rgba(${color.r}, ${color.g}, 255, 1))`;
 
   const types = {
-    h: { name: "Hue", limits: [0, 360, 1], BG: linearGradientHue },
-    s: {
-      name: "Saturation",
-      limits: [0, 100, 1],
-      BG: linearGradientSaturation,
-    },
-    l: { name: "Lightness", limits: [0, 100, 1], BG: linearGradientlightness },
+    h: { name: "Hue", limits: [0, 360, 1], BG: hue },
+    s: { name: "Saturation", limits: [0, 100, 1], BG: saturation },
+    l: { name: "Lightness", limits: [0, 100, 1], BG: lightness },
     r: { name: "Red", limits: [0, 255, 1], BG: linearGradientR },
     g: { name: "Green", limits: [0, 255, 1], BG: linearGradientG },
     b: { name: "Blue", limits: [0, 255, 1], BG: linearGradientB },
@@ -71,7 +56,7 @@ export default function Slider({ type }) {
         "--slider-bg": types[type].BG,
         "--thumb-bg": thumbBG,
       }}
-      value={color.data[type]}
+      value={color[type]}
       onChange={(e) => {
         color.change(type, e.target.value);
       }}
