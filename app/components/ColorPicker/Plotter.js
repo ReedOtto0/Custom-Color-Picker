c83709095e30831146f20e75965419796981e705
ReedOtto0 <153955823+ReedOtto0@users.noreply.github.com>
2024-01-20 16:02:48 -0700

    Reorganized file structure

--- app/components/ColorPicker/Plotter.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useState } from "react";
-import HSL from "./plots/HSL";
-
-export default function Plotter() {
-  const [current, setCurrent] = useState("hsl");
-  return <HSL />;
-}
