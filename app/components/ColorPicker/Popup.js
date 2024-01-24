65c55ebf166a312705a64aae7b0ecbbd7dac1dde
ReedOtto0 <153955823+ReedOtto0@users.noreply.github.com>
2024-01-23 17:57:46 -0700

    Added skeleton of button action panel

--- app/components/ColorPicker/Popup.js
+++ app/components/ColorPicker/Popup.js
@@ -2,7 +2,7 @@
 
 export default function Popup({ open, parentRef, children }) {
   const popupClasses =
-    "w-[300px] p-3 bg-gray-100 border-2 border-gray-300 rounded-xl overflow-hidden absolute left-[-130px] top-[60px]";
+    "w-[300px] pt-2 px-2 bg-gray-100 border-2 border-gray-300 rounded-xl overflow-hidden absolute left-[-130px] top-[60px]";
 
   return (
     <div hidden={!open} className={popupClasses}>
--- app/components/ColorPicker/actionPanel/ActionPanel.js
+++ app/components/ColorPicker/actionPanel/ActionPanel.js
@@ -1,9 +1,13 @@
 import ColorPreview from "../textPanel/ColorPreview";
 
 export default function ActionPanel() {
+  const buttonClasses = `w-1/4`;
   return (
-    <div className="flex flex-row justify-between mt-2">
-      <button></button>
+    <div className="border-t-2 -mx-2 border-gray-300 divide-x-2 divide-gray-300 flex flex-row justify-between mt-2 relative">
+      <button className={buttonClasses}>Pallets</button>
+      <button className={buttonClasses}>Copy</button>
+      <button className={buttonClasses}>Plot</button>
+      <button className={buttonClasses}>Format</button>
     </div>
   );
 }
